dfc9ee2b05615dbeec09ab2449e3f7cd7e50ebf8
Robert Wasinger <robwaz@robwaz.net>
2023-10-21 03:38:04 -0700

    neovim - use newer lua keymap setup

--- neovim/lua/keymappings.lua
+++ neovim/lua/keymappings.lua
@@ -3,39 +3,38 @@ vim.api.nvim_set_keymap('n', '<SPACE>', '<NOP>', OPTS)
 vim.g.mapleader = ' '
 
 -- no hl
-vim.api.nvim_set_keymap('n', '<Leader>h', ':set hlsearch!<CR>', OPTS)
+vim.keymap.set('n', '<Leader>h', ':set hlsearch!<CR>', OPTS)
 
 -- Explorer
-vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', OPTS)
+vim.keymap.set('n', '<Leader>e', ':NvimTreeToggle<CR>', OPTS)
 
 -- Telescope
-vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<cr>', OPTS)
-vim.api.nvim_set_keymap('n', '<Leader>fg', ':Telescope live_grep<cr>', OPTS)
-vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope buffers<cr>', OPTS)
-vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<cr>', OPTS)
+vim.keymap.set('n', '<Leader>ff', ':Telescope find_files<cr>', OPTS)
+vim.keymap.set('n', '<Leader>fg', ':Telescope live_grep<cr>', OPTS)
+vim.keymap.set('n', '<Leader>fb', ':Telescope buffers<cr>', OPTS)
+vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<cr>', OPTS)
 
 -- LSP
-vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', OPTS)
-vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', OPTS)
-vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', OPTS)
-vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', OPTS)
-vim.api.nvim_set_keymap('n', '<Leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', OPTS)
-
+vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', OPTS)
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', OPTS)
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', OPTS)
+vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', OPTS)
+vim.keymap.set('n', '<Leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', OPTS)
 
 -- Better window movement
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { silent = true})
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { silent = true})
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { silent = true})
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { silent = true})
+vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true})
+vim.keymap.set('n', '<C-j>', '<C-w>j', { silent = true})
+vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true})
+vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true})
 
 -- Better indenting
-vim.api.nvim_set_keymap('v', '<', '<gv', OPTS)
-vim.api.nvim_set_keymap('v', '>', '>gv', OPTS)
+vim.keymap.set('v', '<', '<gv', OPTS)
+vim.keymap.set('v', '>', '>gv', OPTS)
 
 -- Better escape
-vim.api.nvim_set_keymap('i', 'jk', '<ESC>', OPTS)
+vim.keymap.set('i', 'jk', '<ESC>', OPTS)
 
 -- Tab switch buffer
-vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', OPTS)
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>', OPTS)
+vim.keymap.set('n', '<TAB>', ':bnext<CR>', OPTS)
+vim.keymap.set('n', '<S-TAB>', ':bprevious<CR>', OPTS)
 
