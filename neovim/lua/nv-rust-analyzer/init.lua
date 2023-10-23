189fab368d7fc4fda244790a89a3dd394e0ba8a5
Robert Wasinger <robwaz@robwaz.net>
2023-10-22 17:36:56 -0700

    neovim - rust-analyzer boilerplate

--- neovim/lua/nv-rust-analyzer/init.lua
+++ neovim/lua/nv-rust-analyzer/init.lua
@@ -1,1 +1,21 @@
-require'lspconfig'.rust_analyzer.setup({})
+require'lspconfig'.rust_analyzer.setup({
+   on_attach=on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
