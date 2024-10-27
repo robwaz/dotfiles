c7cc410eab5a16e8295c1144b345a8609aee801b
Robert Wasinger <robwaz@robwaz.net>
2024-10-26 22:19:17 -0700

    neovim - ts python & indentation

--- neovim/lua/nv-ts/init.lua
+++ neovim/lua/nv-ts/init.lua
@@ -1,11 +1,14 @@
 require("nvim-treesitter.configs").setup {
   ensure_installed = { "bash", "c", "lua", "haskell", "rust", "html", "dockerfile",
-                       "cpp", "diff", "cmake", "bibtex"},
+                       "cpp", "diff", "cmake", "bibtex", "python"},
   sync_install = false,
   auto_install = true,
 
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true
   }
 }
