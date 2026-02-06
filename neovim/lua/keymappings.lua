531e40648623b68b7e7eb6e0fdd9f63349b596da
Robert Wasinger <robwaz@robwaz.net>
2026-02-06 03:48:02 -0700

    add keybind for diagnostic loclist

--- neovim/lua/keymappings.lua
+++ neovim/lua/keymappings.lua
@@ -31,6 +31,7 @@ vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', OPTS)
 vim.keymap.set('n', '<Leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', OPTS)
 vim.keymap.set('n', '<Leader>d', vim.diagnostic.open_float, { desc = "Show diagnostics" })
 vim.keymap.set('n', '<leader>dq', vim.diagnostic.setqflist, { desc = "Diagnostics to QFL" })
+vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = "Diagnostics to LocList" })
 
 -- Better window movement
 vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true})
