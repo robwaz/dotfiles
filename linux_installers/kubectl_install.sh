3f60ea5c73e01f88c9065d8a8850362c47a318b8
Robert Wasinger <robwaz@robwaz.net>
2026-02-06 01:01:13 -0700

    linux kubectl install script

---
+++ linux_installers/kubectl_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
