e844b20e56f424e86b170e55bd735b6b474b97e0
Robert Wasinger <robwaz@robwaz.net>
2026-08-18 04:40:27 +0000

    Neovim - Use new breaking version of tree-sitter

--- neovim/lua/nv-ts/init.lua
+++ neovim/lua/nv-ts/init.lua
@@ -1,14 +1,2 @@
-require("nvim-treesitter.configs").setup {
-  ensure_installed = { "bash", "c", "lua", "haskell", "rust", "html", "dockerfile",
-                       "cpp", "diff", "cmake", "bibtex", "python", "elixir"},
-  sync_install = false,
-  auto_install = true,
-
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true
-  }
-}
+require'nvim-treesitter'.install { "bash", "c", "lua", "haskell", "rust",
+  "html", "dockerfile", "cpp", "diff", "cmake", "bibtex", "python", "elixir"}
